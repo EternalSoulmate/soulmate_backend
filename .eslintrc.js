64d829fde72bbc2f8ed4e983ff1013758d17af6d
dania0x4C <ljm31881004@gmail.com>
2025-02-06 05:09:58 +0900

    [chore] setting docker-compose, prisma, eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  'import/extensions': [
+    'error',
+    'never',
+    {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    },
+  ],
+
   root: true,
   env: {
     node: true,
+    es6: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
@@ -20,6 +34,16 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
   },
 };
